5f4d3e4c4f2b0d4ca3bd64fefc0591148d7b2d53
ftm2-cin <ftm2@cin.ufpe.br>
2023-11-16 22:21:27 -0300

    feat: image picker

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -2,11 +2,40 @@ import { useState } from 'react';
 import { TouchableOpacity } from 'react-native';
 import { Center, ScrollView, VStack, Skeleton, Text} from 'native-base';
 import { ScreenHeader, UserPhoto, Input, Button } from '@components';
+import * as ImagePicker from 'expo-image-picker';
 
 const PHOTO_SIZE = 33;
+
 export default function ProfileScreen() {
 
     const [loading, setLoading] = useState(false);
+    const [photo, setPhoto] = useState('https://avatars.githubusercontent.com/u/119615473?v=4');
+
+    async function handleSelectImage() {
+        try {
+            const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+            if (status !== 'granted') {
+                alert('Eita, precisamos de acesso às suas fotos...');
+                return;
+            }
+            const result = await ImagePicker.launchImageLibraryAsync({
+                allowsEditing: true,
+                quality: 1,
+                mediaTypes: ImagePicker.MediaTypeOptions.Images,
+                aspect: [4, 4],
+            });
+            if (!result.canceled) {
+                setPhoto(result.assets[0].uri);
+            }
+            setLoading(true);
+            setTimeout(() => {
+                setLoading(false);
+            }, 2000);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
         <VStack flex={1}>
             <ScreenHeader title="Perfil"/>
@@ -16,12 +45,12 @@ export default function ProfileScreen() {
                         <Skeleton h={PHOTO_SIZE} w={PHOTO_SIZE} rounded="full" />
                     :
                         <UserPhoto 
-                            source={{ uri: 'https://avatars.githubusercontent.com/u/119615473?v=4'}}
+                            source={{ uri: photo}}
                             alt="User Photo"
                             size={PHOTO_SIZE}
                         />
                     }
-                    <TouchableOpacity>
+                    <TouchableOpacity onPress={handleSelectImage}>
                         <Text color="green.500" fontSize="md" fontWeight="bold" mt={3} mb={8}>Editar foto</Text>
                     </TouchableOpacity>
                     <Input bg="gray.600" placeholder="Nome" />
